fe7e84b635699e7a2c530e7b289ff130720aac8e
Kurobane Yan <yan.kurobane@gmail.com>
2017-06-15 16:22:22 +0300

    remove serviceWorker from project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 import '../node_modules/grommet/grommet-hpinc.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
