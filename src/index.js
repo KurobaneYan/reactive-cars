137035930954f0e753fce40b6215244f0b7886df
Kurobane Yan <yan.kurobane@gmail.com>
2017-06-16 20:44:51 +0300

    remove unnecessary logger in state

--- src/index.js
+++ src/index.js
@@ -2,21 +2,30 @@ import '../node_modules/grommet/grommet-hpinc.min.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import App from './components/App'
+import Admin from './components/Admin'
 import rootReducer from './reducers'
+import createHistory from 'history/createBrowserHistory'
+import { Route, Switch } from 'react-router-dom'
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
+
+const history = createHistory()
+const middleware = routerMiddleware(history)
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
-
-store.subscribe(() =>
-  console.log(store.getState())
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(middleware)
 )
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact path='/' component={App} />
+        <Route path='/admin' component={Admin} />
+      </Switch>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root'))
